61f6843543daa8784f50f1307c6faef4875ed91a
Puneet vishnoi <puneetvishnoi2017@gmail.com>
2023-11-21 20:19:55 +0530

    formatted

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -80,9 +80,16 @@ function CartItem() {
           }
         </ListGroup>
       </div>
+
       <div className='filters summary'>
-        <span className='title'> Subtotal({cart.length}) items</span>
-        <span style={{ fontWeight: 700, fontSize: 20 }}> Total: ${total} </span>
+        <span className='title'> 
+          Subtotal({cart.length}) items
+        </span>
+
+        <span style={{ fontWeight: 700, fontSize: 20 }}> 
+          Total: ${total} 
+        </span>
+
         <Button type='button' disabled={cart.length === 0}>
           Proceed to Checkout
         </Button>
